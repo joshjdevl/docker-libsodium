66ff64812e653281fbd8bbf53c2929a76e46c72b
joshj <joshj>
2014-08-09 11:54:05 -0700

    updating readme and name to libkalium.so

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Android build
 
 Builds libsodium Android static library
 Builds kalium-jni Android shared library
+
+Shared Library and Jar File
+===========================
+Please install docker and then run copy-files-to-host.sh
+
+This will then pull the docker container from [docker-libsodium](https://registry.hub.docker.com/u/joshjdevl/docker-libsodium/) which is a Trusted Build.
--- copy-files-to-host.sh
+++ copy-files-to-host.sh
@@ -1,2 +1,2 @@
-docker run --rm joshjdevl/docker-libsodium cat /installs/kalium-jni/libs/armeabi/libtestjni.so > /tmp/libtestjni.so
+docker run --rm joshjdevl/docker-libsodium cat /installs/kalium-jni/libs/armeabi/libkaliumjni.so > /tmp/libkaliumjni.so
 docker run --rm joshjdevl/docker-libsodium cat /root/.m2/repository/org/abstractj/kalium/kalium-jni/1.0.0-SNAPSHOT/kalium-jni-1.0.0-SNAPSHOT.jar > /tmp/kalium-jni-1.0.0-SNAPSHOT.jar
